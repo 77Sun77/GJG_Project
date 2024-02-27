84aeac59e0172d0b8a4ac5e37b7008be37c88b31
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-02-27 15:56:10 +0900

    feat: hp바가 카메라와 같은 rotation을 가지도록 수정

--- Assets/Scripts/FloatingHealthBar.cs
+++ Assets/Scripts/FloatingHealthBar.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 
 public class FloatingHealthBar : MonoBehaviour
 {
-    [SerializeField]
-    private Slider slider;
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
 
     public void UpdateValue(float current, float max)
     {
         slider.value = current / max;
+    }
+
+    private void Update()
+    {
+        transform.position = target.position + offset;
+        transform.rotation = camera.transform.rotation;
     }
 }
